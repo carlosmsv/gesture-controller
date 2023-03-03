d88fb7e3435e40829838da5928342d2d51fc199a
Carlos <carlos.msv@hotmail.com>
2023-03-03 04:04:56 -0300

    blink counter

--- initial-template/pages/video-player/src/controller.js
+++ initial-template/pages/video-player/src/controller.js
@@ -2,6 +2,7 @@ export default class Controller {
   #view
   #service
   #worker
+  #blinkCounter = 0
   constructor({ view, service, worker }) {
     this.#view = view
     this.#service = service
@@ -15,11 +16,19 @@ export default class Controller {
     controller.log('not yet detecting eye blink! click in the button to start')
     return controller.init()
   }
+
   #configureWorker(worker) {
-    worker.onmessage = (msg) => {
-      if('READY' === msg.data){
+    let ready = false
+    worker.onmessage = ({ data }) => {
+      if('READY' === data){
+        console.log('worker is ready')
         this.#view.enableButton()
+        ready = true
+        return;
       }
+      const blinked = data.blinked
+      this.#blinkCounter += blinked
+      console.log('blinked', blinked)
     }
     return worker
   }
@@ -32,5 +41,6 @@ export default class Controller {
   }
   onBtnStart() {
     this.log('initializing detection...')
+    this.#blinkCounter = 0
   }
 }
