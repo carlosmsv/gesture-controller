33c2338440da671302d080124faf34487d7b6d70
Carlos <carlos.msv@hotmail.com>
2023-03-03 03:00:09 -0300

    using a workerMock if 'type' is not supported

--- initial-template/pages/video-player/src/factory.js
+++ initial-template/pages/video-player/src/factory.js
@@ -4,11 +4,24 @@ import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
 
-if (supportsWorkerType()) {
-  console.log('Supported')
-} else { 
+async function getWorker() {
+  if (supportsWorkerType()) {
+    console.log('Supported')
+    const worker = new Worker( './src/worker.js', { type: 'module'})
+    return worker
+  }
+  
+  const workerMock = {
+    async postMessage() {},
+    onmessage(msg) {}
+  }
+
   console.log('Not supported')
+  return workerMock
 }
+
+const worker = await getWorker()
+
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
